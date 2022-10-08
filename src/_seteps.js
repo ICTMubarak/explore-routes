52fe9d4b4f5468bbde70bda437735f9afbd1b58b
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-10-09 04:28:35 +0600

    Add simple rout

---
+++ src/_seteps.js
@@ -0,0 +1,8 @@
+/*
+
+1. Install react router: npm ireact-router
+2. create a router use createBrowserRouter
+3. Add RouterProvider and pass router props
+4. Create some route inside the router. 
+
+*/
